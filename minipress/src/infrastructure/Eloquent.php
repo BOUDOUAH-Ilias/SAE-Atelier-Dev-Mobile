3d06b0391281cfdbd2a5655eaa115ad1a54dfa79
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2026-06-09 15:14:49 +0200

    Initialisation du projet

--- minipress/src/infrastructure/Eloquent.php
+++ minipress/src/infrastructure/Eloquent.php
@@ -1,35 +1,22 @@
 <?php
-/**
- * File:  Eloquents.php
- * Creation Date: 27/12/2022
- * description: classe Eloquent, service de connexion à la base de données
- *
- * @author: canals
- */
 
-namespace gift\appli\infrastructure;
+declare(strict_types=1);
+
+namespace minipress\infrastructure;
 
 use Illuminate\Database\Capsule\Manager as DB;
 
 class Eloquent
 {
-
-
-    public static function init($filename): void
+    public static function init(string $filename): void
     {
-
-
         $db = new DB();
         $config = parse_ini_file($filename);
-        // Accept both 'dbname' (existing config) and 'database' (Illuminate expected key)
         if (isset($config['dbname']) && !isset($config['database'])) {
             $config['database'] = $config['dbname'];
         }
         $db->addConnection($config);
         $db->setAsGlobal();
         $db->bootEloquent();
-
     }
-
-
 }
