02b96e0256fa00a39ecf518a9f46d0fb99067ad4
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2026-06-09 15:35:28 +0200

    Ajout d'un home basique pour tester la configuration

--- minipress/src/conf/routes.php
+++ minipress/src/conf/routes.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 return function (\Slim\App $app): void {
 
- //   $app->get('/', minipress\webui\actions\GetHomeAction::class)
-//        ->setName('home');
+  $app->get('/', minipress\webui\actions\GetHomeAction::class)
+        ->setName('home');
 };
