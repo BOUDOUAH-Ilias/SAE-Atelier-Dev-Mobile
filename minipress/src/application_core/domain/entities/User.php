2ae184c448683ad1190fd5ab655656994fe02da4
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2026-06-10 10:35:15 +0200

    Ajout de l'entité User.php

---
+++ minipress/src/application_core/domain/entities/User.php
@@ -0,0 +1,15 @@
+<?php
+namespace minipress\application_core\domain\entities;
+
+use Illuminate\Database\Eloquent\Model;
+use minipress\application_core\domain\entities\Article;
+
+class User extends Model{
+
+    public $table = "user";
+    public $timestamps = false;
+
+    public function articles(){
+        return $this->hasMany(Article::class, 'id_auteur');}
+}
+?>
