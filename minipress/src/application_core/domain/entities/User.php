4fcea337b87587866584cf94bf69308ddb648a3f
delat-max <e43496u@etu.univ-lorraine.fr>
2026-06-10 10:54:33 +0200

    Modification de User.php pour pouvoir y vérifier le mot de passe, créer un utilisateur et le trouver avec son email

--- minipress/src/application_core/domain/entities/User.php
+++ minipress/src/application_core/domain/entities/User.php
@@ -6,10 +6,28 @@ use minipress\application_core\domain\entities\Article;
 
 class User extends Model{
 
-    public $table = "user";
+    public $table = "users";
     public $timestamps = false;
+    protected $fillable = ['email', 'password'];
+    protected $hidden = ['password'];
 
     public function articles(){
         return $this->hasMany(Article::class, 'id_auteur');}
+    
+    public function verifyPassword($password) {
+        return password_verify($password, $this->password);
+    }
+    
+    public static function createUser($email, $password) {
+        $user = new self();
+        $user->email = $email;
+        $user->password = password_hash($password, PASSWORD_DEFAULT);
+        $user->save();
+        return $user;
+    }
+    
+    public static function findByEmail($email) {
+        return self::where('email', $email)->first();
+    }
 }
 ?>
