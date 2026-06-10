b8f6344952332efc8fe3d205ea272d125ffc8556
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2026-06-10 10:53:31 +0200

    modify: Ajout de la route slim pour la création d'un article

--- minipress/src/conf/routes.php
+++ minipress/src/conf/routes.php
@@ -6,4 +6,7 @@ return function (\Slim\App $app): void {
 
   $app->get('/', minipress\webui\actions\GetHomeAction::class)
         ->setName('home');
+
+    $app->get('/article/create', minipress\webui\actions\ShowCreateArticleAction::class)
+        ->setName('article.create');
 };
