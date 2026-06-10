a624da78dfdbeb8e7376b00d0bdc4d3d2823c970
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2026-06-10 10:34:49 +0200

    Ajout de l'entité Article.php

---
+++ minipress/src/application_core/domain/entities/Article.php
@@ -0,0 +1,21 @@
+<?php
+namespace minipress\application_core\domain\entities;
+use Illuminate\Database\Eloquent\Model;
+use minipress\application_core\domain\entities\Categorie;
+use minipress\application_core\domain\entities\User;
+
+class Article extends Model{
+
+    public $table = "article";
+
+    public const CREATED_AT = 'date_creation';
+    public const UPDATED_AT = null;
+
+    public function categorie(){
+        return $this->belongsTo(Categorie::class, 'id_categorie');}
+
+    public function auteur(){
+        return $this->belongsTo(User::class, 'id_auteur');}
+
+}
+?>
