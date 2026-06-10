77a3b4d4d7ebbb849b40fbb5e2d1941f840bd0be
Boudouah Ilias <ilias.boudouah8@etu.univ-lorraine.com>
2026-06-10 10:51:28 +0200

    [WIP] API Categories

--- minipress/src/conf/routes.php
+++ minipress/src/conf/routes.php
@@ -6,4 +6,9 @@ return function (\Slim\App $app): void {
 
   $app->get('/', minipress\webui\actions\GetHomeAction::class)
         ->setName('home');
+
+  $app->get('/api/categories', minipress\api\actions\GetCategoriesAction::class)
+        ->setName('api_categories');
+
+  
 };
