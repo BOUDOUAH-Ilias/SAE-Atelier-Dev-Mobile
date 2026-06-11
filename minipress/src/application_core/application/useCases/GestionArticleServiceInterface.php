806d76e16e2a2f79ffa69478ae2b25e8ab5536f5
Boudouah Ilias <ilias.boudouah8@etu.univ-lorraine.com>
2026-06-11 09:47:14 +0200

    [FIX] Nom de methodes

--- minipress/src/application_core/application/useCases/GestionArticleServiceInterface.php
+++ minipress/src/application_core/application/useCases/GestionArticleServiceInterface.php
@@ -10,9 +10,13 @@ interface GestionArticleServiceInterface{
 
     public function getArticleDesc(): array;
 
+<<<<<<< Updated upstream
     public function getArticles(): array;
 
     public function getArticleByCategorie(int $idCategorie): array;
+=======
+        public function getArticles(): array;
+>>>>>>> Stashed changes
 
 
 }
