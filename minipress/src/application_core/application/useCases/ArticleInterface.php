38fd2c2f570b14b47bce8fc5017734a6ebba32e6
Boudouah Ilias <ilias.boudouah8@etu.univ-lorraine.com>
2026-06-11 09:18:34 +0200

    [FIX] Suppression de ArticleService qui étaient en doublon inutilement

--- minipress/src/application_core/application/useCases/ArticleInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace minipress\application_core\application\useCases;
-
-interface ArticleInterface {
-public function getArticles(): array;
-public function getArticleById(int $id): array;
-}
-
