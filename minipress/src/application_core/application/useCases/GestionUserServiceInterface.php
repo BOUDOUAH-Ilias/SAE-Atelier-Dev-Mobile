ff69bcf03f13383642593024e41aa93f8a226c33
delat-max <e43496u@etu.univ-lorraine.fr>
2026-06-12 09:36:49 +0200

    Ajout des lignes manquante de GestionUserServiceInterface

--- minipress/src/application_core/application/useCases/GestionUserServiceInterface.php
+++ minipress/src/application_core/application/useCases/GestionUserServiceInterface.php
@@ -7,5 +7,15 @@ interface GestionUserServiceInterface {
     public function authenticateUser($email, $password);
     public function registerUser($email, $password);
     public function startUserSession(User $user);
+    public function logoutUser();
+    public function isUserAuthenticated();
+    public function getUserById($userId);
+    public function canCreateArticle($userId);
+    public function canEditArticle($userId, $articleId);
+    public function canDeleteArticle($userId, $articleId);
+    public function getUserByEmail($email);
+    public function getCurrentUser();
+    public function emailExists($email);
+    public function updateUserPassword($userId, $newPassword);
 }
 ?>
