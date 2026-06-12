f2a5c8b56981324f4e456f96fa5a2bf0e157ed30
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2026-06-12 11:27:24 +0200

    fix(core): L'utilisateur doit maintenant etre connecté pour créer une catégorie

--- minipress/src/webui/actions/CreateCategorieAction.php
+++ minipress/src/webui/actions/CreateCategorieAction.php
@@ -7,10 +7,18 @@ namespace minipress\webui\actions;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use minipress\application_core\application\useCases\CategorieService;
+use minipress\application_core\application\useCases\GestionUserService;
 
 class CreateCategorieAction{
 
     public function __invoke(Request $request, Response $response): Response{
+        $userService = new GestionUserService();
+        $currentUser = $userService->getCurrentUser();
+
+        if (!$currentUser) {
+            return $response->withHeader('Location', '/signin')->withStatus(302);
+        }
+
         $data = $request->getParsedBody();
 
         $service = new CategorieService();
@@ -18,6 +26,6 @@ class CreateCategorieAction{
             nom: $data['nom'],
         );
 
-        return $response->withHeader('Location', '/')->withStatus(200);
+        return $response->withHeader('Location', '/')->withStatus(302);
     }
 }
--- minipress/src/webui/actions/ShowCreateCategorieAction.php
+++ minipress/src/webui/actions/ShowCreateCategorieAction.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace minipress\webui\actions;
 
 use minipress\application_core\application\useCases\CategorieService;
+use minipress\application_core\application\useCases\GestionUserService;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
@@ -12,6 +13,13 @@ use Slim\Views\Twig;
 class ShowCreateCategorieAction{
 
     public function __invoke(Request $request, Response $response, array $args): Response{
+        $userService = new GestionUserService();
+        $currentUser = $userService->getCurrentUser();
+
+        if (!$currentUser) {
+            return $response->withHeader('Location', '/signin')->withStatus(302);
+        }
+
         $view = Twig::fromRequest($request);
         return $view->render($response, 'form.create.categorie.twig');
     }
