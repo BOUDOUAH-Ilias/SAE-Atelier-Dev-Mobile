663bcae9d330625c6c3aa31d3f9bf9c35117db0b
Hugo Antzorn <hugo.antzorn4@etu.univ-lorraine.fr>
2026-06-12 14:29:37 +0200

    Rajout de health check dans compose.yml pour minipress-appli + un error reporting dans l'index.php pour les erreur vendor

--- minipress/index.php
+++ minipress/index.php
@@ -1,5 +1,6 @@
 <?php
 declare(strict_types=1);
+error_reporting(E_ALL & ~E_DEPRECATED);
 require_once __DIR__ . '/src/vendor/autoload.php';
 $app = require_once __DIR__ . '/src/conf/bootstrap.php';
 $app->run();
