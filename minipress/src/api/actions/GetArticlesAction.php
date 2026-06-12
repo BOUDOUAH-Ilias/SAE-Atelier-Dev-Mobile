757287fbf708c0ff0c956c61a6dd55af8abc7de5
AIME--CABOCEL Léandre <leandreaimecabocel@gmail.com>
2026-06-12 08:58:26 +0200

    Ajout du tri pour l'api

--- minipress/src/api/actions/GetArticlesAction.php
+++ minipress/src/api/actions/GetArticlesAction.php
@@ -7,23 +7,35 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use minipress\application_core\application\useCases\GestionArticleService;
 
-class GetArticlesAction {
+class GetArticlesAction
+{
 
-public function __invoke(Request $rq, Response $rs, array $args){
-    $service = new GestionArticleService();
-    $articles = $service->getArticles();
+    public function __invoke(Request $rq, Response $rs, array $args)
+    {
+        $service = new GestionArticleService();
 
-    $data = [
-            'type'       => 'collection',
-            'count'      => count($articles),
+        $sort = $rq->getQueryParams()['sort'] ?? null;
+
+        $allowed = ['date-asc', 'date-desc', 'auteur'];
+        if ($sort !== null && !in_array($sort, $allowed, true)) {
+            $rs->getBody()->write(json_encode([
+                'error' => 'Valeur de tri invalide. Valeurs acceptées : date-asc, date-desc, auteur'
+            ]));
+            return $rs->withHeader('Content-Type', 'application/json')->withStatus(400);
+        }
+
+        $articles = $service->getArticlesSorted($sort);
+
+        $data = [
+            'type' => 'collection',
+            'count' => count($articles),
             'articles' => array_map(function (array $art) {
                 return [
                     'article' => [
-                        'titre'          => $art['titre'],
-                        'date_creation'     => $art['date_creation'],
-                        'auteur'     => $art['id_auteur'],
+                        'titre' => $art['titre'],
+                        'date_creation' => $art['date_creation'],
+                        'auteur' => $art['id_auteur'],
                     ],
-
                     'links' => [
                         'self' => ['href' => 'api/articles/' . $art['id']],
                     ],
@@ -31,8 +43,8 @@ public function __invoke(Request $rq, Response $rs, array $args){
             }, $articles),
         ];
 
-    $rs->getBody()->write(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+        $rs->getBody()->write(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
 
-    return $rs->withHeader('Content-Type', 'application/json');
-}
+        return $rs->withHeader('Content-Type', 'application/json');
+    }
 }
--- minipress/src/api/actions/GetCategoriesAction.php
+++ minipress/src/api/actions/GetCategoriesAction.php
@@ -7,27 +7,29 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use minipress\application_core\application\useCases\CategorieService;
 
-class GetCategoriesAction {
+class GetCategoriesAction
+{
 
-public function __invoke(Request $rq, Response $rs, array $args){
-    $service = new CategorieService();
-    $categories = $service->getCategories();
+    public function __invoke(Request $rq, Response $rs, array $args)
+    {
+        $service = new CategorieService();
+        $categories = $service->getCategories();
 
-    $data = [
-            'type'       => 'collection',
-            'count'      => count($categories),
+        $data = [
+            'type' => 'collection',
+            'count' => count($categories),
             'categories' => array_map(function (array $cat) {
                 return [
                     'categorie' => [
-                        'id'          => $cat['id'],
-                        'nom'     => $cat['nom'],
+                        'id' => $cat['id'],
+                        'nom' => $cat['nom'],
                     ],
                 ];
             }, $categories),
         ];
 
-    $rs->getBody()->write(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+        $rs->getBody()->write(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
 
-    return $rs->withHeader('Content-Type', 'application/json');
-}
+        return $rs->withHeader('Content-Type', 'application/json');
+    }
 }
